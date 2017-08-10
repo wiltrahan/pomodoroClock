ff985eb59230ed9c1c446c537525c574d39dd97e
wiltrahan <twilorip@gmail.com>
2017-08-10 19:43:18 -0400

    session cdown working

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
   var sessionCount = parseInt($("#session_time").html());
   var breakCount = parseInt($("#break_time").html());
-  var timeClock = parseInt($("#time_clock").html());
+  var timeClock = parseInt($("#time_clock").html(sessionCount));
   $("#reset").hide();
+  $("#work_time, #relax_time").hide();
 
+
+//start button click function
   $("#start").click(function() {
-    var counter = setInterval(timer, 1000);
+    var counter = setInterval(timer, 500);
 
     function timer() {
       $("#start, #subtract_session, #add_session, #subtract_break, #add_break").hide();
-      timeClock -= 1;
-      if(timeClock === 0) {
+      $("#work_time").show();
+      sessionCount -= 1;
+      if(sessionCount === 0) {
         clearInterval(counter);
+        breakTime();
 
       }
 
-      $("#time_clock").html(timeClock);
+      $("#time_clock").html(sessionCount);
     }
   });
 
@@ -24,6 +29,7 @@ $(document).ready(function() {
     if(sessionCount > 5) {
       sessionCount -= 5;
       $("#session_time").html(sessionCount);
+      $("#time_clock").html(sessionCount);
     }
 
   });
@@ -31,6 +37,7 @@ $(document).ready(function() {
   $("#add_session").click(function() {
     sessionCount += 5;
     $("#session_time").html(sessionCount);
+    $("#time_clock").html(sessionCount);
   });
 
   $("#subtract_break").click(function() {
@@ -44,6 +51,15 @@ $(document).ready(function() {
     breakCount += 1;
     $("#break_time").html(breakCount);
   });
+
+  function breakTime() {
+
+    $("#work_time").hide();
+    $("#relax_time").show();
+    $("#time_clock").html(breakCount);
+  }
+
+
 
 
 });
